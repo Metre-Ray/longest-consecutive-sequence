9ca1ea3aa7b8e17ccf0ab0c3bda6b5d2bb388a1a
Metre-Ray <dgary@tut.by>
2018-09-28 17:07:54 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let obj = {};
+  let seq_length = 0;
+  let max_answer = 0;
+
+  if (array.length === 0) return 0;
+  for (let i = 0; i < array.length; i++) {
+    obj[array[i]] = 1;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (obj[array[i] + 1] !== undefined) {
+      seq_length += 1;
+      obj[array[i] + 1] = undefined;
+      let j = 2;
+
+      while(obj[array[i] + j] !== undefined) {
+        seq_length += 1;
+        obj[array[i] + j] = undefined;
+        j += 1;
+      }
+
+      j = 1;
+      while(obj[array[i] - j] !== undefined) {
+        seq_length += 1;
+        obj[array[i] - j] = undefined;
+        j += 1;
+      }
+
+      if (seq_length > max_answer) max_answer = seq_length;
+      seq_length = 0;
+    }
+  }
+
+  return max_answer + 1;
 }
